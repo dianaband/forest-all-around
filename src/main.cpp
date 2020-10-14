7b7bfc7dedafee1b95502e6b8fead7db79d9f70b
Dooho Yi <pilotedeguerre@gmail.com>
2020-10-14 16:41:58 +0900

    new proj.

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@
 //==========</preset>==========
 
 //============<parameters>============
-#define MESH_SSID "cricket-crackers"
+#define MESH_SSID "forest-all/around"
 #define MESH_PASSWORD "cc*vvvv/kkk"
 #define MESH_PORT 5555
 #define MESH_CHANNEL 5
